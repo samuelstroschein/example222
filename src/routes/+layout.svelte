cf2f9065639fc240f07634177fe7774513882f14
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-11-04 17:02:11 -0400

    .

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import ParaglideJsSvelteKitAdapter from "../lib/ParaglideSvelteKit.svelte"
+  import * as runtime from "../paraglide-js/runtime";
+</script>
+
+<ParaglideJsSvelteKitAdapter {...runtime}>
+  <slot></slot>
+</ParaglideJsSvelteKitAdapter>
---
+++ src/routes/[lang]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import "@inlang/paraglide-js"
+	import { setLanguageTag, languageTag } from "../../paraglide-js/runtime"
+	import * as m from "../../paraglide-js/messages"
+</script>
+
+<p>{m.greeting({ name: "Samuel", count: 5 })}</p>
+<p>{m.currentLanguageTag({ languageTag: languageTag() })}</p>
+
+<button on:click={() => setLanguageTag("de")}>change language to "de"</button>
+<button on:click={() => setLanguageTag("en")}>change language to "en"</button>
